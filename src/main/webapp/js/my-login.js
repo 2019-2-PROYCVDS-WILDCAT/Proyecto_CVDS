0622b53c1c2459eca664ef6d47f9f01ce67f0206
AlejandroBohal <sergio.bohorquez@mail.escuelaing.edu.co>
2019-10-31 17:51:24 -0500

    Tarea login, interfaz de usuario loginbean

--- src/main/webapp/js/my-login.js
+++ src/main/webapp/js/my-login.js
@@ -3,15 +3,7 @@
  *
  * Bootstrap 4 Login Page
  *
- * @author          Muhamad Nauval Azhar
- * @uri 			https://nauval.in
- * @copyright       Copyright (c) 2018 Muhamad Nauval Azhar
- * @license         My Login is licensed under the MIT license.
  * @github          https://github.com/nauvalazhar/my-login
- * @version         1.2.0
- *
- * Help me to keep this project alive
- * https://www.buymeacoffee.com/mhdnauvalazhar
  * 
  ******************************************/
 
